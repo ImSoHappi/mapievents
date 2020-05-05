6fdcd07cdf5b1228914f4c09d9e7102ed2f01ce6
samuel <samuelpguzman@hotmail.com>
2020-05-05 18:04:25 -0400

    Arregla carpeta static

---
+++ static/js/circle.js
@@ -0,0 +1,21 @@
+// CODIGO OPEN NAVBAR //
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
+
+$('.post-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  nextArrow: $('.next'),
+  prevArrow: $('.prev'),
+});
---
+++ static/libraries/main.js
@@ -0,0 +1,1 @@
+console.log('Aqui van las librerias');
